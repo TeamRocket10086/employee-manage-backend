62333fc7e3188883ca54780932db9013ac1db97f
George Wang <georgewang2048@gmail.com>
2020-11-18 12:13:38 -0500

    1st baseline: 1. Create all packages 2. Add additional dependencies in maven-wrapper.jar 3. Add settings for database

---
+++ src/main/java/com/bfs/employemanagesys/EmployemanagesysApplication.java
@@ -0,0 +1,13 @@
+package com.bfs.employemanagesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployemanagesysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployemanagesysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bfs/employemanagesys/EmployemanagesysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfs.employemanagesys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployemanagesysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
