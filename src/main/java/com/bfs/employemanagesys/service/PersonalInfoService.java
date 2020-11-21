ec6d1f19009cd4df8c1f7a04625e94e70fad6d1c
George Wang <georgewang2048@gmail.com>
2020-11-20 19:01:35 -0500

    1. Add module for personal info. 2. Refine a few pojo and dao.

---
+++ src/main/java/com/bfs/employemanagesys/service/PersonalInfoService.java
@@ -0,0 +1,54 @@
+package com.bfs.employemanagesys.service;
+
+import com.bfs.employemanagesys.dao.AddressDAO;
+import com.bfs.employemanagesys.dao.NewContactDAO;
+import com.bfs.employemanagesys.dao.PersonDAO;
+import com.bfs.employemanagesys.pojo.Address;
+import com.bfs.employemanagesys.pojo.Contact;
+import com.bfs.employemanagesys.pojo.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class PersonalInfoService {
+    public PersonDAO personDAO;
+    public AddressDAO addressDAO;
+    public NewContactDAO contactDAO;
+
+    @Autowired
+    public void setNewContactDAO(NewContactDAO cDAO) {
+        this.contactDAO = cDAO;
+    }
+
+    @Autowired
+    public void setPersonDAO(PersonDAO pDAO) {
+        this.personDAO = pDAO;
+    }
+
+    @Autowired
+    public void setAddressDAO(AddressDAO aDAO) { this.addressDAO = aDAO; }
+
+    @Transactional
+    public List<Address> getAddresses(int pid) {
+        Person person = personDAO.getPersonById(pid);
+        return addressDAO.getAddressesByPerson(person);
+    }
+
+    @Transactional
+    public List<Contact> getContacts(int pid) {
+        Person person = personDAO.getPersonById(pid);
+        return contactDAO.getContactsByPerson(person);
+    }
+
+    @Transactional
+    public void updatePersonByEmailOrPhone(int pid, String email, String phone, String altphone) {
+        Person person = personDAO.getPersonById(pid);
+        person.setEmail(email);
+        person.setCellPhone(phone);
+        person.setAlternatePhone(altphone);
+        personDAO.addPerson(person);
+    }
+}
